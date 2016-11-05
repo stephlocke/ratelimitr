7f4ce7c919c864784b3f5573db1deebdd63e4cea
tarakc02 <tarakc02@users.noreply.github.com>
2016-11-05 10:48:50 -0700

    more policies

--- R/policies.R
+++ R/policies.R
@@ -1,8 +1,41 @@
-wait <- function(tokens, exception) {
-    Sys.sleep(exception$wait_time)
-    request(tokens, policy = wait)
+#' @import assertthat
+#' @export
+policy <- function(fun, policy_name) {
+    assert_that(is.string(policy_name),
+                is.function(fun),
+                identical(names(formals(fun)), c("tokens", "exception")))
+
+    # to be a valid policy, the function should return a logical value
+    toks <- token_dispenser(1, .03)
+    exc <- rate_limit_exception(.01)
+    res <- fun(toks, exc)
+    if (!is.flag(res))
+        stop("policy must be a function that returns a logical(1)")
+
+    structure(fun,
+              policy_name = policy_name,
+              class = c("policy", class(fun)))
 }
 
-drop <- function(tokens, exception) {
-    FALSE
+#' @export
+policy_wait <-
+    structure(
+        function(tokens, exception) {
+            Sys.sleep(exception$wait_time)
+            request(tokens, policy = policy_wait)
+        }, policy_name = "wait", class = c("policy", "function")
+    )
+
+#' @export
+policy_drop <-
+    structure(
+        function(tokens, exception) {
+            FALSE
+        }, policy_name = "drop", class = c("policy", "function")
+    )
+
+#' @export
+print.policy <- function(pol) {
+    cat("A ratelimitr policy called:", attr(pol, "policy_name"), "\n")
+    invisible(pol)
 }
